d67bb6e1dc9b0f9d7e11400fec2ec3320f492f11
Amiya Behera <amiyatulu@gmail.com>
2025-03-01 19:07:51 +0530

    conviction-voting

--- pallets/conviction-voting/src/types.rs
+++ pallets/conviction-voting/src/types.rs
@@ -1,128 +1,235 @@
+// This file is part of Substrate.
+
+// Copyright (C) Parity Technologies (UK) Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Miscellaneous additional datatypes.
+
+use crate::{AccountVote, Conviction, Vote, VoteThreshold};
 use codec::{Decode, DecodeWithMemTracking, Encode, MaxEncodedLen};
 use scale_info::TypeInfo;
 use sp_runtime::{
-    traits::{Bounded, CheckedDiv, CheckedMul, Zero},
-    RuntimeDebug,
-    Perbill,
+	traits::{Bounded, CheckedAdd, CheckedDiv, CheckedMul, CheckedSub, Saturating, Zero},
+	RuntimeDebug,
 };
 
-pub type ProposalId = u32;
+/// A proposal index.
+pub type PropIndex = u32;
 
-#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo)]
-pub enum ProposalType<AccountId, Balance> {
-    Grant {
-        recipient: AccountId,
-        amount: Balance,
-    },
-    ValidatorApplication {
-        validator: AccountId,
-        commission: Perbill,
-    },
-}
+/// A referendum index.
+pub type ReferendumIndex = u32;
 
-#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo)]
-pub struct Proposal<AccountId, Balance, BlockNumber> {
-    pub proposer: AccountId,
-    pub proposal_type: ProposalType<AccountId, Balance>,
-    pub stake: Balance,
-    pub start_block: BlockNumber,
-    pub end_block: BlockNumber,
-    pub tally: Tally<Balance>,
-    pub voters: Vec<AccountId>,
-}
-
-#[derive(Encode, Decode, Clone, PartialEq, Eq, RuntimeDebug, Default, TypeInfo)]
+/// Info regarding an ongoing referendum.
+#[derive(Encode, MaxEncodedLen, Decode, Default, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo)]
 pub struct Tally<Balance> {
-    pub total_power: Balance,
-    pub in_favor: Balance,
-    pub against: Balance,
+	/// The number of aye votes, expressed in terms of post-conviction lock-vote.
+	pub ayes: Balance,
+	/// The number of nay votes, expressed in terms of post-conviction lock-vote.
+	pub nays: Balance,
+	/// The amount of funds currently expressing its opinion. Pre-conviction.
+	pub turnout: Balance,
 }
 
-// Conviction voting implementation
-#[derive(Encode, Decode, Copy, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
-pub enum Conviction {
-    None,
-    Locked1x,
-    Locked2x,
-    Locked3x,
-    Locked4x,
-    Locked5x,
-    Locked6x,
+/// Amount of votes and capital placed in delegation for an account.
+#[derive(
+	Encode, MaxEncodedLen, Decode, Default, Copy, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo,
+)]
+pub struct Delegations<Balance> {
+	/// The number of votes (this is post-conviction).
+	pub votes: Balance,
+	/// The amount of raw capital, used for the turnout.
+	pub capital: Balance,
 }
 
-impl Default for Conviction {
-	fn default() -> Self {
-		Conviction::None
-	}
-}
-
-impl From<Conviction> for u8 {
-	fn from(c: Conviction) -> u8 {
-		match c {
-			Conviction::None => 0,
-			Conviction::Locked1x => 1,
-			Conviction::Locked2x => 2,
-			Conviction::Locked3x => 3,
-			Conviction::Locked4x => 4,
-			Conviction::Locked5x => 5,
-			Conviction::Locked6x => 6,
+impl<Balance: Saturating> Saturating for Delegations<Balance> {
+	fn saturating_add(self, o: Self) -> Self {
+		Self {
+			votes: self.votes.saturating_add(o.votes),
+			capital: self.capital.saturating_add(o.capital),
 		}
 	}
-}
 
+	fn saturating_sub(self, o: Self) -> Self {
+		Self {
+			votes: self.votes.saturating_sub(o.votes),
+			capital: self.capital.saturating_sub(o.capital),
+		}
+	}
 
-impl TryFrom<u8> for Conviction {
-	type Error = ();
-	fn try_from(i: u8) -> Result<Conviction, ()> {
-		Ok(match i {
-			0 => Conviction::None,
-			1 => Conviction::Locked1x,
-			2 => Conviction::Locked2x,
-			3 => Conviction::Locked3x,
-			4 => Conviction::Locked4x,
-			5 => Conviction::Locked5x,
-			6 => Conviction::Locked6x,
-			_ => return Err(()),
-		})
+	fn saturating_mul(self, o: Self) -> Self {
+		Self {
+			votes: self.votes.saturating_mul(o.votes),
+			capital: self.capital.saturating_mul(o.capital),
+		}
+	}
+
+	fn saturating_pow(self, exp: usize) -> Self {
+		Self { votes: self.votes.saturating_pow(exp), capital: self.capital.saturating_pow(exp) }
 	}
 }
 
+impl<
+		Balance: From<u8>
+			+ Zero
+			+ Copy
+			+ CheckedAdd
+			+ CheckedSub
+			+ CheckedMul
+			+ CheckedDiv
+			+ Bounded
+			+ Saturating,
+	> Tally<Balance>
+{
+	/// Create a new tally.
+	pub fn new(vote: Vote, balance: Balance) -> Self {
+		let Delegations { votes, capital } = vote.conviction.votes(balance);
+		Self {
+			ayes: if vote.aye { votes } else { Zero::zero() },
+			nays: if vote.aye { Zero::zero() } else { votes },
+			turnout: capital,
+		}
+	}
 
-impl Conviction {
-    pub fn lock_periods(self) -> u32 {
-        match self {
-            Conviction::None => 0,
-            Conviction::Locked1x => 1,
-            Conviction::Locked2x => 2,
-            Conviction::Locked3x => 4,
-            Conviction::Locked4x => 8,
-            Conviction::Locked5x => 16,
-            Conviction::Locked6x => 32,
-        }
-    }
+	/// Add an account's vote into the tally.
+	pub fn add(&mut self, vote: AccountVote<Balance>) -> Option<()> {
+		match vote {
+			AccountVote::Standard { vote, balance } => {
+				let Delegations { votes, capital } = vote.conviction.votes(balance);
+				self.turnout = self.turnout.checked_add(&capital)?;
+				match vote.aye {
+					true => self.ayes = self.ayes.checked_add(&votes)?,
+					false => self.nays = self.nays.checked_add(&votes)?,
+				}
+			},
+			AccountVote::Split { aye, nay } => {
+				let aye = Conviction::None.votes(aye);
+				let nay = Conviction::None.votes(nay);
+				self.turnout = self.turnout.checked_add(&aye.capital)?.checked_add(&nay.capital)?;
+				self.ayes = self.ayes.checked_add(&aye.votes)?;
+				self.nays = self.nays.checked_add(&nay.votes)?;
+			},
+		}
+		Some(())
+	}
 
-    pub fn voting_power<Balance: From<u8> + CheckedMul + CheckedDiv + Bounded + Zero>(
-        self,
-        balance: Balance,
-    ) -> Balance {
-        match self {
-            Conviction::None => balance.checked_div(&10u8.into()).unwrap_or_else(Zero::zero),
-            x => balance
-                .checked_mul(&u8::from(x).into())
-                .unwrap_or_else(Bounded::max_value),
-        }
-    }
+	/// Remove an account's vote from the tally.
+	pub fn remove(&mut self, vote: AccountVote<Balance>) -> Option<()> {
+		match vote {
+			AccountVote::Standard { vote, balance } => {
+				let Delegations { votes, capital } = vote.conviction.votes(balance);
+				self.turnout = self.turnout.checked_sub(&capital)?;
+				match vote.aye {
+					true => self.ayes = self.ayes.checked_sub(&votes)?,
+					false => self.nays = self.nays.checked_sub(&votes)?,
+				}
+			},
+			AccountVote::Split { aye, nay } => {
+				let aye = Conviction::None.votes(aye);
+				let nay = Conviction::None.votes(nay);
+				self.turnout = self.turnout.checked_sub(&aye.capital)?.checked_sub(&nay.capital)?;
+				self.ayes = self.ayes.checked_sub(&aye.votes)?;
+				self.nays = self.nays.checked_sub(&nay.votes)?;
+			},
+		}
+		Some(())
+	}
+
+	/// Increment some amount of votes.
+	pub fn increase(&mut self, approve: bool, delegations: Delegations<Balance>) -> Option<()> {
+		self.turnout = self.turnout.saturating_add(delegations.capital);
+		match approve {
+			true => self.ayes = self.ayes.saturating_add(delegations.votes),
+			false => self.nays = self.nays.saturating_add(delegations.votes),
+		}
+		Some(())
+	}
+
+	/// Decrement some amount of votes.
+	pub fn reduce(&mut self, approve: bool, delegations: Delegations<Balance>) -> Option<()> {
+		self.turnout = self.turnout.saturating_sub(delegations.capital);
+		match approve {
+			true => self.ayes = self.ayes.saturating_sub(delegations.votes),
+			false => self.nays = self.nays.saturating_sub(delegations.votes),
+		}
+		Some(())
+	}
 }
 
+/// Info regarding an ongoing referendum.
+#[derive(Encode, MaxEncodedLen, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo)]
+pub struct ReferendumStatus<BlockNumber, Proposal, Balance> {
+	/// When voting on this referendum will end.
+	pub end: BlockNumber,
+	/// The proposal being voted on.
+	pub proposal: Proposal,
+	/// The thresholding mechanism to determine whether it passed.
+	pub threshold: VoteThreshold,
+	/// The delay (in blocks) to wait after a successful referendum before deploying.
+	pub delay: BlockNumber,
+	/// The current tally of votes in this referendum.
+	pub tally: Tally<Balance>,
+}
 
-impl Bounded for Conviction {
-	fn min_value() -> Self {
-		Conviction::None
-	}
-	fn max_value() -> Self {
-		Conviction::Locked6x
+/// Info regarding a referendum, present or past.
+#[derive(Encode, MaxEncodedLen, Decode, Clone, PartialEq, Eq, RuntimeDebug, TypeInfo)]
+pub enum ReferendumInfo<BlockNumber, Proposal, Balance> {
+	/// Referendum is happening, the arg is the block number at which it will end.
+	Ongoing(ReferendumStatus<BlockNumber, Proposal, Balance>),
+	/// Referendum finished at `end`, and has been `approved` or rejected.
+	Finished { approved: bool, end: BlockNumber },
+}
+
+impl<BlockNumber, Proposal, Balance: Default> ReferendumInfo<BlockNumber, Proposal, Balance> {
+	/// Create a new instance.
+	pub fn new(
+		end: BlockNumber,
+		proposal: Proposal,
+		threshold: VoteThreshold,
+		delay: BlockNumber,
+	) -> Self {
+		let s = ReferendumStatus { end, proposal, threshold, delay, tally: Tally::default() };
+		ReferendumInfo::Ongoing(s)
 	}
 }
 
+/// Whether an `unvote` operation is able to make actions that are not strictly always in the
+/// interest of an account.
+pub enum UnvoteScope {
+	/// Permitted to do everything.
+	Any,
+	/// Permitted to do only the changes that do not need the owner's permission.
+	OnlyExpired,
+}
 
-
+/// Identifies an owner of a metadata.
+#[derive(
+	Encode,
+	Decode,
+	DecodeWithMemTracking,
+	Clone,
+	PartialEq,
+	Eq,
+	RuntimeDebug,
+	TypeInfo,
+	MaxEncodedLen,
+)]
+pub enum MetadataOwner {
+	/// External proposal.
+	External,
+	/// Public proposal of the index.
+	Proposal(PropIndex),
+	/// Referendum of the index.
+	Referendum(ReferendumIndex),
+}
